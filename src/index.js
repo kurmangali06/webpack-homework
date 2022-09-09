f3d782103a076ccdcadf55b01211c21de1876d2a
Курмангали <kurmangali0606@gmail.com>
2022-09-09 20:41:45 +0600

    1

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import('./main.scss')
-
-import { drawGalleryItem } from "./item";
-import items from "./items";
-
-const galleryRootElement = document.getElementById('gallery')
-
-items.map(item => galleryRootElement.appendChild(drawGalleryItem(item)))
